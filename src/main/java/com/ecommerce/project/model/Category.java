0076add9ca40e07bc001760869f52449babd5703
Sonal Tamse <sonal.tamse@gmail.com>
2026-02-16 23:53:25 +0530

    Integrate Spring Data JPA for Category entity and refactor all CRUD operations

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,12 +1,25 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.aspectj.lang.annotation.control.CodeGenerationHint;
+
+@Entity(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
     public Category(String categoryName, Long categoryId) {
         this.categoryName = categoryName;
         this.categoryId = categoryId;
+    }
+
+    public Category() {
+
     }
 
     public Long getCategoryId() {
