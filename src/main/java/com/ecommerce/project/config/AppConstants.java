45df7ea6a58b101c414d41690275fffdcb187bcc
Sonal Tamse <sonal.tamse@gmail.com>
2026-02-18 23:42:12 +0530

    feat(category): add pagination with default values and standardize responses using APIResponse; update createCategory to use request params; remove echo endpoint

--- src/main/java/com/ecommerce/project/config/AppConstants.java
+++ src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -2,4 +2,5 @@ package com.ecommerce.project.config;
 
 public class AppConstants {
     public static final String PAGE_NUMBER = "0", PAGE_SIZE="50";
+    public static final String SORT_DIR = "asc",SORT_CATEGORIES_BY = "categoryId";
 }
--- src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.exceptions;
 
+import com.ecommerce.project.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -24,15 +25,17 @@ public class MyGlobalExceptionHandler{
     }
 
     @ExceptionHandler(ResourceNotfoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotfoundException res){
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotfoundException res){
         String message = res.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(APIException res){
+    public ResponseEntity<APIResponse> myAPIException(APIException res){
         String message = res.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
     }
 }
 
