01957b0f7aa8ec4ac3a8afc29a5ac99dd0946e6f
Sonal Tamse <sonal.tamse@gmail.com>
2026-03-22 12:09:51 +0530

    feat(product): add delete product API

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -44,4 +44,10 @@ public class ProductController {
         ProductDTO updatedProductDTO = productService.updateProduct(product, productId);
         return new ResponseEntity<>(updatedProductDTO, HttpStatus.OK);
     }
+
+    @DeleteMapping("/admin/products/{productId}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId){
+        ProductDTO deletedProduct = productService.deleteProduct(productId);
+        return new ResponseEntity<>(deletedProduct,HttpStatus.OK);
+    }
 }
--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -77,4 +77,11 @@ public class ProductServiceImpl implements ProductService{
 //        return it as a DTO with modelMapper
         return modelMapper.map(savedProduct, ProductDTO.class);
     }
+
+    @Override
+    public ProductDTO deleteProduct(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(()-> new ResourceNotfoundException("Product","productId", productId));
+        productRepository.delete(product);
+        return modelMapper.map(product,ProductDTO.class);
+    }
 }
