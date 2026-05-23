ddcfc3c4a34c699cdb0e88bf39c8bd7980532e0c
Sonal Tamse <sonal.tamse@gmail.com>
2026-05-23 09:54:28 +0530

    (feat): add users and relationships and using uniqueContraints attribute

---
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -0,0 +1,16 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    private String street;
+    private String city;
+    private String state;
+}
---
+++ src/main/java/com/ecommerce/project/model/AppRole.java
@@ -0,0 +1,5 @@
+package com.ecommerce.project.model;
+
+public enum AppRole {
+    ROLE_DEVELOPER, ROLE_SELLER, ROLE_ADMIN
+}
