8bac3a47ea3db3dc359fb784002b270b62c8561b
Sonal Tamse <sonal.tamse@gmail.com>
2026-06-04 09:04:25 +0530

    completing RoleRepository and covering loose ends

--- src/main/java/com/ecommerce/project/repositories/RoleRepository.java
+++ src/main/java/com/ecommerce/project/repositories/RoleRepository.java
@@ -1,7 +1,14 @@
 package com.ecommerce.project.repositories;
 
+import com.ecommerce.project.model.AppRole;
+import com.ecommerce.project.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.lang.ScopedValue;
+import java.util.Optional;
+
 @Repository
-public class RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(AppRole appRole);
 }
