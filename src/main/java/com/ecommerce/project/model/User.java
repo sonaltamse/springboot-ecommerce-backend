7f769a6a7028496dea3a8ee7544e299cab706f0f
Sonal Tamse <sonal.tamse@gmail.com>
2026-06-09 19:27:19 +0530

    relationships with cart

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -64,4 +64,8 @@ public class User {
     @JoinTable(name = "user_addresses", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses = new ArrayList<>();
+
+    @ToString.Exclude
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private Cart cart;
 }
