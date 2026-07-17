a896385db38f4ed0fb2ba8c533323447bfa57131
Sonal Tamse <sonal.tamse@gmail.com>
2026-07-17 20:00:48 +0530

    feat: configure AWS RDS PostgreSQL and fix project build

--- src/main/java/com/ecommerce/project/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/ecommerce/project/security/jwt/AuthEntryPointJwt.java
@@ -9,8 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
--- src/main/java/com/ecommerce/project/security/services/UserDetailsImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsImpl.java
@@ -3,12 +3,10 @@ package com.ecommerce.project.security.services;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -53,7 +51,7 @@ public class UserDetailsImpl implements UserDetails {
     }
 
     @Override
-    public @Nullable String getPassword() {
+    public String getPassword() {
         return password;
     }
 
