fbda790c062406e93f5616ce7891ce02c10266db
Sonal Tamse <sonal.tamse@gmail.com>
2026-07-19 11:14:12 +0530

    customize swagger documentation

--- src/main/java/com/ecommerce/project/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/project/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.ecommerce.project.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +22,15 @@ public class SwaggerConfig {
         SecurityRequirement bearerRequirement = new SecurityRequirement().
                 addList("bearerAuth");
 
-        return new OpenAPI().components(new Components().addSecuritySchemes("bearerAuth", bearerScheme))
+        return new OpenAPI().
+                info(new Info().title("E-commerce API")
+                        .version("1.0")
+                        .description("API documentation for the E-commerce application")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .contact(new Contact()
+                                .name("Sonal Tamse")
+                                .email("sonal@gmail.com")))
+                .components(new Components().addSecuritySchemes("bearerAuth", bearerScheme))
                 .addSecurityItem(bearerRequirement);
     }
 }
